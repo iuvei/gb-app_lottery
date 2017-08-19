3b03a97bae82a5266d02f20904ed8b1fd7355b49
cherry <cherry1012@mail.com>
2017-08-19 17:13:16 +0800

    ==添加站点彩票

--- lottery/java/so/wwb/gamebox/lottery/hall/controller/LotteryResultHistoryController.java
+++ lottery/java/so/wwb/gamebox/lottery/hall/controller/LotteryResultHistoryController.java
@@ -1,19 +1,21 @@
 package so.wwb.gamebox.lottery.hall.controller;
 
+import org.soul.commons.collections.CollectionTool;
+import org.soul.commons.collections.MapTool;
 import org.soul.commons.lang.string.StringTool;
 import org.soul.commons.query.sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import so.wwb.gamebox.lottery.session.SessionManager;
 import so.wwb.gamebox.lottery.tools.ServiceTool;
-import so.wwb.gamebox.model.company.enums.GameStatusEnum;
+import so.wwb.gamebox.model.TerminalEnum;
 import so.wwb.gamebox.model.company.lottery.po.*;
 import so.wwb.gamebox.model.company.lottery.vo.LotteryFrequencyListVo;
 import so.wwb.gamebox.model.company.lottery.vo.LotteryHandicapVo;
 import so.wwb.gamebox.model.company.lottery.vo.LotteryResultListVo;
 import so.wwb.gamebox.web.cache.Cache;
 
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -37,32 +39,25 @@ public class LotteryResultHistoryController {
     public String toLotteryResultHistory(LotteryResultListVo listVo, Model model) {
         Map<String, LotteryFrequencyType> lotteryFrequencyType = Cache.getLotteryFrequencyType();
         model.addAttribute("lotteryFrequencyType", lotteryFrequencyType);
-        Map<String, Lottery> lottery = Cache.getLottery();
-        buildResultData(listVo, lottery, model);
+        Map<String, SiteLottery> siteLotteryMap = Cache.getNormalSiteLotteryMap(TerminalEnum.PC.getCode(), SessionManager.getSiteId());
+        buildResultData(listVo, siteLotteryMap, model);
         return "/hall/LotteryHistory";
     }
 
-    private void buildResultData(LotteryResultListVo listVo, Map<String, Lottery> lottery, Model model) {
+    private void buildResultData(LotteryResultListVo listVo, Map<String, SiteLottery> siteLotteryMap, Model model) {
         String code = "";
         String type = "";
-        model.addAttribute("lotterys", lottery);
-        if (StringTool.isBlank(listVo.getSearch().getCode())) {
-            if (lottery != null) {
-                Iterator<String> iterator = lottery.keySet().iterator();
-                while (iterator.hasNext()) {
-                    String key = iterator.next();
-                    Lottery lot = lottery.get(key);
-                    if (GameStatusEnum.NORMAL.getCode().equals(lot.getStatus())) {
-                        code = key;
-                        type = lot.getType();
-                        break;
-                    }
-                }
+        model.addAttribute("lotterys", siteLotteryMap);
+        if (StringTool.isBlank(listVo.getSearch().getCode()) && MapTool.isNotEmpty(siteLotteryMap)) {
+            for (SiteLottery siteLottery : siteLotteryMap.values()) {
+                code = siteLottery.getCode();
+                type = siteLottery.getType();
+                break;
             }
             listVo.getSearch().setCode(code);
         } else {
             code = listVo.getSearch().getCode();
-            type=lottery.get(code).getType();
+            type = siteLotteryMap.get(code).getType();
         }
         listVo.getQuery().addOrder(LotteryResult.PROP_OPEN_TIME, Direction.DESC);
         listVo.getQuery().addOrder(LotteryResult.PROP_EXPECT, Direction.DESC);
@@ -73,7 +68,7 @@ public class LotteryResultHistoryController {
         model.addAttribute("type", type);
         model.addAttribute("command", listVo);
         Map<Object, LotteryHandicap> expectMaps = ServiceTool.lotteryHandicapService().queryLotteryExpectCount(new LotteryHandicapVo());
-        model.addAttribute("expectMaps",expectMaps);
+        model.addAttribute("expectMaps", expectMaps);
     }
 
     @RequestMapping(value = "/queryLotteryResultByCode")
@@ -93,42 +88,41 @@ public class LotteryResultHistoryController {
         model.addAttribute("code", listVo.getSearch().getCode());
         model.addAttribute("type", listVo.getSearch().getType());
         Map<Object, LotteryHandicap> expectMaps = ServiceTool.lotteryHandicapService().queryLotteryExpectCount(new LotteryHandicapVo());
-        model.addAttribute("expectMaps",expectMaps);
+        model.addAttribute("expectMaps", expectMaps);
         return url;
     }
 
     @RequestMapping(value = "/queryLotteryByFrequency")
     public String queryLotteryByFrequency(LotteryFrequencyListVo listVo, Model model) {
-        Map<String, Lottery> lottery = Cache.getLottery();
+        Map<String, SiteLottery> siteLotteryMap = Cache.getNormalSiteLotteryMap(TerminalEnum.PC.getCode(), SessionManager.getSiteId());
         if (listVo.getSearch().getLotteryFrequencyId() == null) {
             LotteryResultListVo resultListVo = new LotteryResultListVo();
-            buildResultData(resultListVo, lottery, model);
+            buildResultData(resultListVo, siteLotteryMap, model);
         } else {
             listVo = ServiceTool.lotteryFrequencyService().search(listVo);
-            if (listVo.getResult() != null && listVo.getResult().size() > 0) {
+            if (CollectionTool.isNotEmpty(listVo.getResult())) {
                 LotteryResultListVo resultListVo = new LotteryResultListVo();
-                Map<String, Lottery> stringLotteryMap = removeNoExistLottery(listVo, lottery);
+                Map<String, SiteLottery> stringLotteryMap = removeNoExistLottery(listVo, siteLotteryMap);
                 buildResultData(resultListVo, stringLotteryMap, model);
             }
         }
 
-        //lotterys
         return "/hall/result/AllLottery";
     }
 
-    private Map<String, Lottery> removeNoExistLottery(LotteryFrequencyListVo listVo, Map<String, Lottery> lottery) {
-        Map<String, Lottery> lotteryMap = new LinkedHashMap<>();
-        for (Map.Entry<String, Lottery> entry : lottery.entrySet()) {
-            String key = entry.getKey();
-            Lottery lot = entry.getValue();
+    private Map<String, SiteLottery> removeNoExistLottery(LotteryFrequencyListVo listVo, Map<String, SiteLottery> siteLotteryMap) {
+        Map<String, SiteLottery> lotteryMap = new LinkedHashMap<>();
+        for (Map.Entry<String, SiteLottery> entry : siteLotteryMap.entrySet()) {
+            String code = entry.getKey();
+            SiteLottery siteLottery = entry.getValue();
             boolean exist = false;
             for (LotteryFrequency frequency : listVo.getResult()) {
-                if (lot.getId().equals(frequency.getLotteryId())) {
+                if (siteLottery.getId().equals(frequency.getLotteryId())) {
                     exist = true;
                 }
             }
             if (exist) {
-                lotteryMap.put(key, lot);
+                lotteryMap.put(code, siteLottery);
             }
 
         }
