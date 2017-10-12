c39dfe7a393ecd49fc0b3b6b88653cd108a093d2
marz <zhuzhidu784>
2017-10-12 19:35:48 +0800

    pc端玩家首页未登录时,跳转到指定彩种

--- lottery/java/so/wwb/gamebox/lottery/controller/IndexController.java
+++ lottery/java/so/wwb/gamebox/lottery/controller/IndexController.java
@@ -1,13 +1,14 @@
 package so.wwb.gamebox.lottery.controller;
 
 import org.json.JSONObject;
+import org.soul.commons.enums.EnumTool;
 import org.soul.commons.init.context.CommonContext;
 import org.soul.commons.lang.string.I18nTool;
-import org.soul.commons.lang.string.StringTool;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import so.wwb.gamebox.model.enums.lottery.LotteryEnum;
 import so.wwb.gamebox.web.cache.Cache;
 import so.wwb.gamebox.web.lottery.controller.LotteryDemoController;
 
@@ -22,10 +23,11 @@ public class IndexController  extends LotteryDemoController {
     private static final String INDEX_URI = "index";
 
     @RequestMapping(value = "index")
-    protected String index(Model model, String type, String code) {
-        if(StringTool.isNotEmpty(type) && StringTool.isNotEmpty(code)){
-            model.addAttribute("type", type);
-            model.addAttribute("code", code);
+    protected String index(Model model, String code) {
+        LotteryEnum lotteryEnum = EnumTool.enumOf(LotteryEnum.class,code);
+        if(lotteryEnum != null){
+            model.addAttribute("type", lotteryEnum.getType());
+            model.addAttribute("code", lotteryEnum.getCode());
         }
         return INDEX_URI;
     }
