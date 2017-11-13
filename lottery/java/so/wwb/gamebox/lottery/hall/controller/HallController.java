4903d225ef0a8b53e24b5fdc7df8883b020fa9e2
marz <zhuzhidu784>
2017-11-13 18:14:27 +0800

    1.玩家中心登录按钮根据不同彩票站跳转不同请求路径 2.boss修复撤单撤销方式 3.boss修复以前旧撤单方法，避免被使用时，出现数据问题

--- lottery/java/so/wwb/gamebox/lottery/hall/controller/HallController.java
+++ lottery/java/so/wwb/gamebox/lottery/hall/controller/HallController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import so.wwb.gamebox.lottery.session.SessionManager;
 import so.wwb.gamebox.lottery.tools.ServiceTool;
+import so.wwb.gamebox.model.ParamTool;
 import so.wwb.gamebox.model.TerminalEnum;
 import so.wwb.gamebox.model.company.lottery.po.LotteryFrequency;
 import so.wwb.gamebox.model.company.lottery.po.LotteryResult;
@@ -70,6 +71,8 @@ public class HallController extends BaseLotteryController {
                 model.addAttribute("type", type);
                 model.addAttribute("code", code);
             }
+            //是否是纯彩票站
+            model.addAttribute("isLotterySite", ParamTool.isLotterySite(SessionManager.getSiteId()));
         }
         return HALL_INDEX_URL;
     }
