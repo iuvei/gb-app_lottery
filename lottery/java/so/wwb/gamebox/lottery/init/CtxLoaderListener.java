9e6b39830370b355fb022e69b985cfa4154161eb
tony <tony@wwb.so>
2017-11-27 22:53:54 +0800

    Merge branch 'v2001' of git.gbboss.com:gb/base into v2001
    # Conflicts:
    #	webcommon/java/so/wwb/gamebox/web/shiro/common/filter/KickoutFilter.java

--- lottery/java/so/wwb/gamebox/lottery/init/CtxLoaderListener.java
+++ lottery/java/so/wwb/gamebox/lottery/init/CtxLoaderListener.java
@@ -2,16 +2,14 @@ package so.wwb.gamebox.lottery.init;
 
 import so.wwb.gamebox.web.init.CommonCtxLoaderListener;
 
-import javax.servlet.ServletContextEvent;
-
 /**
  * Created by Kevice on 2015/3/23 0023.
  */
 public class CtxLoaderListener extends CommonCtxLoaderListener {
 
     @Override
-    public void customInit(ServletContextEvent event) throws Exception {
-        super.customInit(event);
+    public void customInit() throws Exception {
+        super.customInit();
         PassportContextPath="";
     }
 }
